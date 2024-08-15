2b6c6b6ee8604f548738381fbc4901f7504088bd
cahyoajadah <lordcahyo76@gmail.com>
2024-08-15 21:47:39 +0700

    bikin startup matcha

--- js/script.js
+++ js/script.js
@@ -44,3 +44,23 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+// transition sections
+
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+
+        if(top >= offset && top < offset + height){
+            sec.classList.add('show-animate');
+        }
+        else{
+            sec.classList.remove('show-animate');
+        }
+    })
+}
